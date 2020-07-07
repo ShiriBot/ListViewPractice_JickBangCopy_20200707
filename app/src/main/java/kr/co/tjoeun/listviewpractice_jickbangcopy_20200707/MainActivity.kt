4eb1a0b1345d9d0e6b42aacf4e24d2b1fd9ccd94
채정실 <cowjdtlf@gmail.com>
2020-07-07 21:55:07 +0900

    목록 내용 추가, 어댑터 연결...?

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
@@ -2,11 +2,14 @@ package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.adapters.RoomAdapter
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
 
 class MainActivity : BaseActivity() {
 
     val mRoomList = ArrayList<Room>()
+    lateinit var mRoomAdapter :RoomAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +30,9 @@ class MainActivity : BaseActivity() {
         mRoomList.add(Room(48000,"서울시 강서구",-2,"여섯번째 방입니다."))
         mRoomList.add(Room(96000,"서울시 종로구",0,"일곱번째 방입니다."))
         mRoomList.add(Room(44000,"서울시 종로구",6,"여덞번째 방입니다."))
+
+        mRoomAdapter = RoomAdapter(mContext,R.layout.room_list_item,mRoomList)
+        roomListView.adapter = mRoomAdapter
 
     }
 
