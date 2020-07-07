4af0f8548283cb668d3af0ec6f512682573c4cbe
채정실 <cowjdtlf@gmail.com>
2020-07-07 20:33:46 +0900

    BaseActivity 세틸

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
@@ -3,9 +3,17 @@ package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setvalues()
     }
+    override fun setupEvents() {
+    }
+
+    override fun setvalues() {
+    }
+
 }
