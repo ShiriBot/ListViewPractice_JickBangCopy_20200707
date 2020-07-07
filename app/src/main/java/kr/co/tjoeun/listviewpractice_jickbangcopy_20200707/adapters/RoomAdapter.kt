afed3ec2db169307e14af01f6e8545b87d2a1c1b
채정실 <cowjdtlf@gmail.com>
2020-07-07 22:01:17 +0900

    실제 데이터를 일다 있는 그래도 출력  -> 보기좋게 가공할 필요가 있다.

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/adapters/RoomAdapter.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/adapters/RoomAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.R
 import kr.co.tjoeun.listviewpractice_jickbangcopy_20200707.datas.Room
 
@@ -22,6 +23,16 @@ class RoomAdapter(val mContext: Context, val resId:Int, val mList:List<Room>):
         }
 
         val row = temprow !!
+
+        val data = mList[position]
+
+        val priceTxt = row.findViewById<TextView>(R.id.priceTxt)
+        val adressTxt = row.findViewById<TextView>(R.id.adressTxt)
+        val desTxt = row.findViewById<TextView>(R.id.desTxt)
+
+        priceTxt.text= data.price.toString()
+        adressTxt.text= data.address.toString()
+        desTxt.text= data.description.toString()
 
         return row
 
