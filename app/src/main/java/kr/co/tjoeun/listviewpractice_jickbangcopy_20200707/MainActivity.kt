90375fac931654f44335372d38bbbac27d8383ed
채정실 <cowjdtlf@gmail.com>
2020-07-08 20:45:10 +0900

    Room클래스를 serializable 인터페이스를 구현해서, putExtra 가능하도록. (받아주는 부분까지)

--- app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/listviewpractice_jickbangcopy_20200707/MainActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.tjoeun.listviewpractice_jickbangcopy_20200707
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -23,6 +24,12 @@ class MainActivity : BaseActivity() {
             val clickedRoom = mRoomList[position]
 
 //            눌린 방의 정보를 상세화면에 전달.
+            val myIntent = Intent(mContext,ViewRoomDetailActivity::class.java)
+
+//            putExtra로 방 정보 첨부 => 방 데이터를 한꺼번에 첨부
+            myIntent.putExtra("room",clickedRoom)
+
+            startActivity(myIntent)
         }
 
     }
